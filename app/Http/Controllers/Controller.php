0a469dcee1af74ef6fc5dba6568887e53ce4898d
ukyo0724 <ukyd1129@gmail.com>
2024-01-16 02:37:04 +0900

    初期設定

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Post;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index(Post $post){
+        return $post->get();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 
+Route::get('/posts', [PostController::class, 'index']);   
 /*
 |--------------------------------------------------------------------------
 | Web Routes
