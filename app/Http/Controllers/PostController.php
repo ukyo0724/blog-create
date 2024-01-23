3269a22e137025a4924c88c27ed5c5ab4aa1a502
ukyo0724 <ukyd1129@gmail.com>
2024-01-24 03:54:15 +0900

    create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 class PostController extends Controller
 {
     public function index(Post $post){
-        return view('posts/index')->with(['posts'=> $post->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts'=> $post->getPaginateByLimit(1)]);
     }
     public function show(Post $post){
         return view('posts/show')->with(['post'=>$post]);
@@ -15,5 +16,10 @@ class PostController extends Controller
     }
     public function create(){
         return view('posts/create');
+    }
+    public function store(PostRequest $request, Post $post){
+        $input=$request['post'];
+        $post->fill($input)->save();
+        return redirect("/posts/$post->id");
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,10 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'title',
+        'body',
+        ];
     public function getByLmit(int $limit_count=10){
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
-    public function getPaginateByLimit(int $limit_count=10){
+    public function getPaginateByLimit(int $limit_count=5){
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
