7ecb6778c9cbee5427c52488198e9c2485ebc07e
ukyo0724 <ukyd1129@gmail.com>
2024-01-23 17:37:20 +0900

    create

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    public function getByLimit(int $limit_count=10){
+    public function getByLmit(int $limit_count=10){
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
     public function getPaginateByLimit(int $limit_count=10){
